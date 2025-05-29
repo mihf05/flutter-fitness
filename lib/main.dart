1415b82af948392e472ba21755aee140388d8f99
mihf05 <fahimimran0088@gmail.com>
2025-05-29 20:10:08 +0600

    Update app title from 'GymApp Demo' to 'GymApp'

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'GymApp Demo',
+      title: 'GymApp',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         textTheme: GoogleFonts.bricolageGrotesqueTextTheme(),
